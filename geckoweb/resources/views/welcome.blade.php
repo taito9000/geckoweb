729b402377080c55a8b2bddfd5482fb1e5465c9b
taito9000 <ada.0205.tai120@gmail.com>
2022-12-14 21:49:09 +0900

    221214_topPageBEM

--- geckoweb/resources/views/welcome.blade.php
+++ geckoweb/resources/views/welcome.blade.php
@@ -2,32 +2,47 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        @vite('resources/scss/app.scss', 'resources/js/app.js','resources/scss/topPageMenu.scss','resources/scss/pageTitle.scss')
+        @vite('geckoweb\resources\scss\app.scss', 'resources/js/app.js')
         <title>Laravel</title>
     </head>
     <body>
-
-            <div class="video-Box">
-                <video class="video-box__video" muted autoplay loop playsinline>
-                    <source src="/public\storage\video\reptile.mp4" type="video/mp4" >
+        <header class="header">
+            <div class="videoBox">
+                <video class="videoBox__video" muted autoplay loop playsinline>
+                    <source src="{{ asset('/video/reptile.mp4')}}" type="video/mp4" >
                 </video>
             </div>
-            <div class="topPageMenu">
-                <div class="topPageMenu__leo">
-                    <a href="">
-                        <img src="{{ asset('geckoweb\public\storage\photo\leo0002 (2).jpg')}}">
-                    </a>
-                </div>
-                <p><a href="">INTRO</a></p>
-                <p><a href="">飼育方法</a></p>
-                <p><a href="">品種紹介</a></p>
-                <p><a href="">質問板</a></p>
-            </div>
-        
-            <div class=pageTitle>ヒョウモントカゲモドキ<br/>飼育入門</div>
-        
 
-            <button class="convince">他の爬虫類の画像見る</div>
-  
+            <div class="topPageMenu">
+
+                <ul class="topPageMenu__ul">
+                    <li class="topPageMenu__leo">
+                        <a href="">
+                            <img  class="topPageMenu__leoimg" src="{{ asset('/photo/leo0002 (2).jpg')}}">
+                        </a>
+                    </li>
+                    <li class="topPageMenu__item">
+                        <a class="topPageMenu__link" href="">INTRO</a>
+                    </li>
+                    <li class="topPageMenu__item">
+                        <a class="topPageMenu__link" href="">飼育方法</a>
+                    </li>
+                    <li class="topPageMenu__item">
+                        <a class="topPageMenu__link" href="">品種紹介</a>
+                    </li>
+                    <li class="topPageMenu__item">
+                        <a class="topPageMenu__link" href="">質問板</a>
+                    </li>
+                </ul>
+        
+                <h1 class = "pageTitle">ヒョウモントカゲモドキ<br/>飼育入門</h1>
+            </div>
+
+        </header>
+
+        <main>
+            <button class="convince">他の爬虫類の画像見る</button>
+        </main>
+
     </body>
 </html>
